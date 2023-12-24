fcf6152f594db0050e9e48f2b182bb92cac534de
Luis Angel Ponce Alvarez <poncealvarezluisangel4@gmail.com>
2023-12-23 19:58:01 -0600

    añado configuración del sitio

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./Style/main.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { Error404 } from "./Components/Error/404.jsx";
+import App from "./App.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const routes = [
+  {
+    path: "/",
+    component: <App />,
+  },
+  {
+    path: "/*",
+    element: <Error404 />,
+  },
+];
+
+const router = createBrowserRouter(routes);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
